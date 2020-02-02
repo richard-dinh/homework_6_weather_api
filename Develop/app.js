95e093032b10532ae9b45dec1ea20ad322ca1b64
richard-dinh <richarddinh@gmail.com>
2020-02-01 19:23:10 -0800

    start function to get data from api call

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,10 @@ let userInput
 
 let pastCities = []
 
+//  API KEY : 504fb55759317621b3658208c57633c9
 
+
+//API call by city name: api.openweathermap.org/data/2.5/weather?q=<cityName>
 document.addEventListener('click', ()=>{
   let target = event.target
   if(target.classList.contains('btn')){
@@ -10,5 +13,15 @@ document.addEventListener('click', ()=>{
     //saving user input into pastCities array
     pastCities.push(userInput)
     console.log(userInput)
+    getWeather(userInput)
   }
 })
+
+const getWeather = userInput =>{
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=504fb55759317621b3658208c57633c9`)
+  .then(response => response.json())
+  .then(data =>{
+    console.log(data)
+  })
+  .catch(error => console.error(error))
+}
