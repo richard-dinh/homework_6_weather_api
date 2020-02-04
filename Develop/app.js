72d71eecefddc51a03b8d670190512e0d4dc96dc
richard-dinh <richarddinh@gmail.com>
2020-02-03 19:39:38 -0800

    wrote getFiveDayForecast function

--- Develop/app.js
+++ Develop/app.js
@@ -2,6 +2,7 @@ let userInput
 let uvIndex
 let pastCities = []
 let display = document.getElementById('cityInfo')
+let fiveSelect= document.getElementById('fiveDay')
 
 //  API KEY : 504fb55759317621b3658208c57633c9
 
@@ -18,6 +19,10 @@ document.addEventListener('click', ()=>{
   }
 })
 
+const toFarenheit = value =>{
+  value = (value * (9 / 5) - 459.67).toFixed(2)
+  return value
+}
 //Capitalize first letter of every word in the string
 const titleCase = str => {
   let splitStr = str.toLowerCase().split(' ');
@@ -37,16 +42,18 @@ const getCityWeather = userInput =>{
     .then(({ main: { temp, humidity }, wind: { speed }, coord: { lon, lat } }) => {
       userInput = titleCase(userInput)
       let info = document.createElement('div')
-      temp = (temp * (9 / 5) - 459.67).toFixed(2)
+      temp = toFarenheit(temp)
 
       // console.log(temp, humidity, speed, lon, lat)
-      info.innerHTML = `<h2>${userInput} </h2>
-    <p>Temperature: ${temp} deg F</p>
+      info.innerHTML = `<h2>${userInput} ${moment().format('MM/DD/YYYY')}</h2>
+    <p>Temperature: ${temp} ºF</p>
     <p>Humidity: ${humidity}</p>
     <p> Wind Speed: ${speed} mph </p>
     `
       display.append(info)
       getUvIndex(lon, lat)
+      getFiveDayForecast(lon, lat)
+      
     })
     .catch(error => console.error(error))
 }
@@ -72,5 +79,28 @@ const getUvIndex = (lon, lat) =>{
       display.append(uvNode)
     })
 
+    .catch(error => console.error(error))
+}
+
+const getFiveDayForecast = (lon, lat) =>{
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=504fb55759317621b3658208c57633c9`)
+    .then(response => response.json())
+    .then(({list}) => {
+      console.log(list)
+      //converting unix time stamp to a date time
+      console.log(moment.unix(list[0].dt).format("MM/DD/YYYY"))
+      for(let i = 7; i<list.length; i+=7){
+        let fiveNode = document.createElement('div')
+        fiveNode.setAttribute('class', 'col-sm-2 fiveDayStyle')
+        fiveNode.innerHTML =`
+            <h6>${moment.unix(list[i].dt).format("MM/DD/YYYY")}</h6>
+            <p>Temp: ${toFarenheit(list[i].main.temp)} ºF</p>
+            <p>Humidity: ${list[i].main.humidity}%</p>
+        `
+        console.log(fiveNode)
+        fiveSelect.append(fiveNode)
+      }
+      // starts at index 0 and increases by 7
+    })
     .catch(error => console.error(error))
 }
