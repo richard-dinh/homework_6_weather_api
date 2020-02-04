d41c488f7388bb1cfab2b09def04242767674d7e
richard-dinh <richarddinh@gmail.com>
2020-02-03 18:25:56 -0800

    created getCityWeather function

--- Develop/app.js
+++ Develop/app.js
@@ -18,26 +18,7 @@ document.addEventListener('click', ()=>{
   }
 })
 
-const getWeather = userInput =>{
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=504fb55759317621b3658208c57633c9`)
-  .then(response => response.json())
-  .then(({main: {temp, humidity}, wind:{speed}, coord:{lon, lat}}) =>{
-    userInput = titleCase(userInput)
-    let info = document.createElement('div')
-    temp = (temp *(9/5) - 459.67).toFixed(2)
-    
-    // console.log(temp, humidity, speed, lon, lat)
-    info.innerHTML = `<h2>${userInput} </h2>
-    <p>Temperature: ${temp} deg F</p>
-    <p>Humidity: ${humidity}</p>
-    <p> Wind Speed: ${speed} mph </p>
-    `
-    display.append(info)
-    getUvIndex(lon, lat)
-  })
-  .catch(error => console.error(error))
-}
-
+//Capitalize first letter of every word in the string
 const titleCase = str => {
   let splitStr = str.toLowerCase().split(' ');
   // console.log(splitStr)
@@ -46,7 +27,29 @@ const titleCase = str => {
   }
   return splitStr.join(' ');
 }
+const displayWeather = userInput =>{
+  getCityWeather(userInput)
+}
 
+const getCityWeather = userInput =>{
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=504fb55759317621b3658208c57633c9`)
+    .then(response => response.json())
+    .then(({ main: { temp, humidity }, wind: { speed }, coord: { lon, lat } }) => {
+      userInput = titleCase(userInput)
+      let info = document.createElement('div')
+      temp = (temp * (9 / 5) - 459.67).toFixed(2)
+
+      // console.log(temp, humidity, speed, lon, lat)
+      info.innerHTML = `<h2>${userInput} </h2>
+    <p>Temperature: ${temp} deg F</p>
+    <p>Humidity: ${humidity}</p>
+    <p> Wind Speed: ${speed} mph </p>
+    `
+      display.append(info)
+      getUvIndex(lon, lat)
+    })
+    .catch(error => console.error(error))
+}
 const getUvIndex = (lon, lat) =>{
   fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=504fb55759317621b3658208c57633c9&lat=${lat}&lon=${lon}`)
     .then(response => response.json())
